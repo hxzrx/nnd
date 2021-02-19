b31df147381dd503fabe4e4bebb4261347135de7
hxz <herongxian@163.com>
2021-02-19 13:09:57 +0800

    fixed many bugs about lmbp, and then pass the exercise E12.16

--- bp-variations.lisp
+++ bp-variations.lisp
@@ -9,6 +9,11 @@
              :type list
              :initform nil
              :documentation "jacobian matrix")
+   (error-vector :initarg :error-vector
+                 :accessor error-vector
+                 :type list
+                 :initform nil
+                 :documentation "a Q*Sᴹ vector, appended by the errors of all the samples")
    (marquardt-sensitive :initarg :marquardt-sensitive
                         :accessor marquardt-sensitive
                         :type list
@@ -45,9 +50,7 @@
 (defmethod initialize-instance :after ((lmbp lmbp-network) &key &allow-other-keys)
   ""
   (flet ((layer-param-sum (a) (* (cdr a) (1+ (car a))))) ; add 1 to denote the bias num
-    (with-accessors (;(neurons neuron-nums)
-                     (parameters parameter-num)) lmbp
-      ;(setf neurons (loop for w in (weights lmbp) collect (if (numberp w) 1 (length w))))
+    (with-accessors ((parameters parameter-num)) lmbp
       (setf parameters (loop for w in (weights lmbp) sum (if (numberp w) 2 (layer-param-sum (matrix-size w))))))))
 
 
@@ -135,7 +138,6 @@ returns (cons new-point new-alpha)"
       (format t "~&i: ~d, α=~f, γ=~f, x=~d, F(x)=~f~%" i α γ x Fx)
       )))
 
-
 (defun reset-squared-error-sum! (lmbp)
   "reset squared-error-sum of lmbp to zero"
   (setf (squared-error-sum lmbp) 0))
@@ -143,27 +145,13 @@ returns (cons new-point new-alpha)"
 (defun update-squared-error-sum! (lmbp samples)
   "update squared-error-sum of lmbp"
   (reset-squared-error-sum! lmbp)
-  (loop for sample in samples
-        do (progn
-             (let ((a (propagation-forward-without-states lmbp (if (numberp (car sample))
-                                                                  (car sample)
-                                                                  (list (car sample)))))
-                   (target (if (numberp (cadr sample)) (cadr sample) (transpose (list (cadr sample))))))
-               (incf (squared-error-sum lmbp) (inner-product-self (matrix-sub target a))))))
+  (with-slots ((sse squared-error-sum)) lmbp
+    (loop for (input target) in samples
+          do (progn
+               (let ((a (propagation-forward-without-states lmbp (list-to-vector input)))
+                     (tg (list-to-vector target)))
+               (incf sse (inner-product-self (matrix-sub tg a)))))))
   (squared-error-sum lmbp))
-
-
-
-(defun parameter-type-list (weights biases)
-  "return a list of flatten type such as (:w :w :w :w :b :b) to denote an element of a row of Jacobian matrix corresponding to a weight or a bias"
-  (when weights
-    (append (append (if (numberp (car weights)) (list :w)
-                        (make-list (* (car (matrix-size (car weights))) (cdr (matrix-size (car weights))))
-                                   :initial-element :w))
-                    (if (numberp (car biases)) (list :b)
-                        (make-list (car (matrix-size (car biases)))
-                                   :initial-element :b)))
-            (parameter-type-list (cdr weights) (cdr biases)))))
 
 (defun parameters-to-list (network)
   "collect the elements of weights and biases of all layers of a network instance and transfer them into a row vector,
@@ -201,7 +189,6 @@ note this return a row vector, not column vector"
     (setf (biases network) new-biases)
     (list new-weights new-biases)))
 
-
 (defmethod marquardt-sensitivity-init (derivative net-inputs)
   "(12.46), Marquardt sensitivity, for the last layer, Sᴹ = -1 ∂Fᴹ(nᴹ),
 it's a matrix, not a column vector compared to bp's sensitivity"
@@ -223,7 +210,6 @@ it's a matrix, not a column vector compared to bp's sensitivity"
 Second, flat this matrix and get an row vector.
 Third, append weights' Jacobian row vector and biases' Jacobian row vector and get an row vector.(This step is executed in other place.)
 The steps above get a row corresponding to one final output's component of partial derivative, so loop over the sensitivity matrix, we will get the full jacobian matrix' block on this layer."
-  ;(format t "~&sens: ~d, weights: ~d, inputs: ~d, tpye: ~d~%" marquardt-sensitivity weights inputs type)
   (let* ((sensitivity (if (numberp marquardt-sensitivity) (list (list marquardt-sensitivity)) marquardt-sensitivity))
          (weight (if (numberp weights) (list (list weights)) weights))
          (input (if (numberp inputs) (list (list inputs)) inputs)))
@@ -237,20 +223,25 @@ The steps above get a row corresponding to one final output's component of parti
           ((eq type :bias) (matrix-flatten (transpose sensitivity)))
           (t (error "Unknown type in function jacoban-block")))))
 
-(defmethod calc-jacobian% ((lmbp lmbp-network) (sample list))
-  "calc the rows of the jacobian matrix corresponding to one sample"
-
-  (let* ((a (propagation-forward lmbp (if (numberp (car sample))
-                                          (car sample)
-                                          (list (car sample)))));propagation forward
+(defmethod calc-jacobian!% ((lmbp lmbp-network) (sample list))
+  "calc the rows of the jacobian matrix corresponding to one sample,
+side effect: will modify error-vector slot of lmbp, and will modidify squared-error-sum slot of lmbp"
+  (let* ((a (propagation-forward lmbp (list-to-vector (first sample)))) ;;propagation forward
          (weight-list (reverse (weights lmbp)))
          (bias-list (reverse (biases lmbp)))
          (input-list (inputs lmbp))
          (net-input-list (net-inputs lmbp))
          (dF (reverse (derivatives lmbp)))
-         (target (if (numberp (cadr sample)) (cadr sample) (transpose (list (cadr sample)))))
+         (target (list-to-vector (second sample)))
          )
-    (incf (squared-error-sum lmbp) (inner-product-self (matrix-sub target a)))
+    (with-slots ((err error-vector)
+                 (sse squared-error-sum)) lmbp
+      (let ((e (matrix-sub target a)))
+        (if (listp e) ;update error-vector
+            (setf err (append err e))
+            (setf err (append err (list (list e)))))
+        (incf sse (inner-product-self e))))
+
     (do* ((idx (1- (length (weights lmbp))) (decf idx))
           (next-weight nil (car cur-weights))
           (cur-weights weight-list (cdr cur-weights))
@@ -267,63 +258,69 @@ The steps above get a row corresponding to one final output's component of parti
           (jacobian-block (make-augmented jacobian-block-weight jacobian-block-bias)
                           (make-augmented (make-augmented jacobian-block-weight jacobian-block-bias)
                                           jacobian-block))
-
           )
          ((= idx 0)
-          ;;(format t "~&jacobian-block: ~d~%" jacobian-block)
           (setf (jacobian lmbp) (append (jacobian lmbp) jacobian-block))
-          (jacobian lmbp))
-      ;;(format t "~&jacobian-block: ~d, jacobian-matrix: ~d~%" jacobian-block jacobian-matrix)
-      )))
+          (jacobian lmbp)))))
 
-
-(defmethod calc-jacobian ((lmbp lmbp-network) (samples list))
+(defmethod calc-jacobian! ((lmbp lmbp-network) (samples list))
   "append jacobian matrix' blocks with respect to each sample, and get the final jacobian matrix,
-the side effect is to write into the jacobian slot of `lmbp"
+the side effect is to write into the jacobian slot of `lmbp, and will modify error-vector slot of lmbp, and will modidify squared-error-sum slot of lmbp"
+  ;;reset to nil and then update the value in calc-jacobian!%
+  (setf (error-vector lmbp) nil)
+  (setf (jacobian lmbp) nil)
+  (reset-squared-error-sum! lmbp)
+
   (dolist (sample samples (jacobian lmbp))
-    (calc-jacobian% lmbp sample))
+    (calc-jacobian!% lmbp sample))
   (jacobian lmbp))
 
-
-(defun hessian-approximation (hessian mu)
+(defun hessian-approximation-inv (hessian mu)
   "Hessian matrix may not be invertible, so modificate it to the approximate Hessian matrix: G = H + μI, where μ>0.
 `mu will be summed up on each recursion until the matrix G=H+k*μI is invertable"
   (alexandria:if-let ((inv (matrix-inverse hessian)))
     inv
-    (hessian-approximation (matrix-add hessian (matrix-product mu (eye (length hessian)))) mu)))
+    (hessian-approximation-inv (matrix-add hessian (matrix-product mu (eye (length hessian)))) mu)))
 
-(defun levenberg-marquardt-backpropagation (lmbp samples mu theta &optional (tolerance 0.01))
-  "levenberg marquardt backpropagation algorithm, page 224, Chinese ed."
-  (let ((θ theta))
-    (do* ((μ mu mu)
-          (x (transpose (parameters-to-list lmbp))
-             (transpose (parameters-to-list lmbp)))
-          (jacobian (calc-jacobian lmbp samples)
-                    (calc-jacobian lmbp samples))
+(defun levenberg-marquardt-backpropagation (lmbp samples &optional (mu 0.01) (theta 10) (tolerance 0.01))
+  "levenberg marquardt backpropagation algorithm, page 224, Chinese ed. mu and theta's default value is suggested in page 222"
+  (let ((θ theta)
+        (x (transpose (parameters-to-list lmbp)))
+        (μ mu))
+    ;;the outer do* executes step 1 and 2 in the algorithm
+    (do* ((j 1 (incf j))
+          (jacobian (calc-jacobian! lmbp samples) ;J(x) in equation (12.23), this step also gets error vector and squared error sum
+                    (calc-jacobian! lmbp samples))
+          (error-vector (error-vector lmbp) ;v(x) in equations (12.20) and (12.22) and others
+                        (error-vector lmbp))
           (JᵀJ (matrix-product (transpose jacobian) jacobian)
                (matrix-product (transpose jacobian) jacobian))
-          (gradient (reduce #'matrix-product (list 2 (transpose jacobian) x))
-                    (reduce #'matrix-product (list 2 (transpose jacobian) x)))
+          (gradient (reduce #'matrix-product (list 2 (transpose jacobian) error-vector)) ;equation (12.22)
+                    (reduce #'matrix-product (list 2 (transpose jacobian) error-vector)))
           )
          ((<(norm gradient) tolerance)
-          (format t "~&lmbp converged. Gradient norm: ~f~%" (norm gradient))
+          (format t "~&LMBP Converged in <~d> iterations! Gradient norm: ~,5f, SSE: ~,5f~%" j (norm gradient) (squared-error-sum lmbp))
           lmbp)
-      (do* ((hessian-approx-inv (hessian-approximation JᵀJ μ)
-                                (hessian-approximation JᵀJ μ))
-            (Δx (reduce #'matrix-product (list -1 hessian-approx-inv (transpose jacobian) x))
-                (reduce #'matrix-product (list -1 hessian-approx-inv (transpose jacobian) x)))
-            (new-x (matrix-add x Δx) (matrix-add x Δx))
+      (format t "~&SSE: ~,4f, Gradient Norn: ~,4f~%" (squared-error-sum lmbp) (norm gradient))
+      ;; the inner do* executes step 3 and 4 in the algorithm
+      (do* ((i 0 (incf i))
+            (hessian-approx-inv (hessian-approximation-inv JᵀJ μ)
+                                (hessian-approximation-inv JᵀJ μ))
+            (Δx (reduce #'matrix-product (list -1 hessian-approx-inv (transpose jacobian) error-vector)) ; equation (12.32)
+                (reduce #'matrix-product (list -1 hessian-approx-inv (transpose jacobian) error-vector)))
+            (new-x (matrix-add x Δx) (matrix-add x Δx)) ; equation (12.31)
             (old-weights (weights lmbp) (weights lmbp))
             (old-biases (biases lmbp) (biases lmbp))
-            (dummy (list-to-parameters! lmbp new-x) (list-to-parameters! lmbp new-x))
-            (old-squared-error-sum (squared-error-sum lmbp) (squared-error-sum lmbp))
+            (old-squared-error-sum (squared-error-sum lmbp) (squared-error-sum lmbp)) ;F(x), computed when the jacobian was computed
+            (dummy (list-to-parameters! lmbp (car (transpose new-x))) ;update weights and biases
+                   (list-to-parameters! lmbp (car (transpose new-x))))
             (new-squared-error-sum (update-squared-error-sum! lmbp samples)
                                    (update-squared-error-sum! lmbp samples))
-            (new-gradient (reduce #'matrix-product (list 2 (transpose jacobian) x))
-                          (reduce #'matrix-product (list 2 (transpose jacobian) x))))
-           ((< new-squared-error-sum old-squared-error-sum)
-            (setf μ (* μ θ))
+            )
+           ((< new-squared-error-sum old-squared-error-sum) ;step 4
+            (setf μ (/ μ θ))
             (setf x new-x))
+        ;;(format t "~&Old SSE: ~,3f, new SSE: ~,3f~%" old-squared-error-sum new-squared-error-sum)
         (setf μ (* μ θ))
         (setf (weights lmbp) old-weights)
         (setf (biases lmbp) old-biases))
@@ -346,7 +343,8 @@ the side effect is to write into the jacobian slot of `lmbp"
 (defun example-12.3 ()
   "page 231, P12.3"
   (let ((f (quadratic-function '((2 0) (0 50))))
-        (g (list #'(lambda (x1 x2) (* 2 x1)) #'(lambda (x1 x2) (* 50 x2)))))
+        (g (list #'(lambda (x1 x2) (declare (ignore x2)) (* 2 x1))
+                 #'(lambda (x1 x2) (declare (ignore x1)) (* 50 x2)))))
     (variable-learning-rate f g '((0.5) (0.5)) 0.05 0.2 1.5 0.5 0.05)))
 
 (defun example-12.5 ()
@@ -356,8 +354,8 @@ the side effect is to write into the jacobian slot of `lmbp"
                                  :transfer-list (list #'square #'purelin)
                                  :derivative-list (list :square :purelin)))
         (data '((1 1) (2 2))))
-    (format t "~&Jacobian matrix for the firse input ~d: ~d~%" (first data) (calc-jacobian% lmbp (first data)))
-    (format t "~&Jacobian matrix after one more input ~d: ~d~%" (second data) (calc-jacobian% lmbp (second data)))
+    (format t "~&Jacobian matrix for the firse input ~d: ~d~%" (first data) (calc-jacobian!% lmbp (first data)))
+    (format t "~&Jacobian matrix after one more input ~d: ~d~%" (second data) (calc-jacobian!% lmbp (second data)))
     ))
 
 (defun example-12.5+ ()
@@ -367,7 +365,7 @@ the side effect is to write into the jacobian slot of `lmbp"
                                   :transfer-list (list #'square #'purelin)
                                   :derivative-list (list :square :purelin)))
          (data '((1 1) (2 2)))
-         (res (calc-jacobian lmbp data)))
+         (res (calc-jacobian! lmbp data)))
     (format t "~&Train set: ~d~&Result: ~d~%" data res)
     ))
 
@@ -378,5 +376,24 @@ the side effect is to write into the jacobian slot of `lmbp"
                                   :transfer-list (list #'logsig #'purelin)
                                   :derivative-list (list :logsig :purelin)))
          (data (data-generator-accurate #'(lambda (x) (1+ (sin (* (/ pi 2) x)))) 1 0 2))
-         (jacobian (calc-jacobian lmbp data)))
+         (jacobian (calc-jacobian! lmbp data)))
     (format t "~&Jacobian matrix:~&~d~%" jacobian)))
+
+(defun exercise-12.16 ()
+  "page 240, Chinese ed. The net structure is the same as exercise 11.25"
+  (let* ((sdbp (make-bp-network :neuron-list (list 1 10 1)
+                                :transfer-list (list #'logsig #'purelin)
+                                :derivative-list (list :logsig :purelin)))
+         (lmbp (make-lmbp-network :neuron-list (list 1 10 1)
+                                  :transfer-list (list #'logsig #'purelin)
+                                  :derivative-list (list :logsig :purelin)))
+         (data (data-generator-accurate #'(lambda (x) (1+ (sin (* (/ pi 4) x)))) -2 2 11)))
+    (declare (ignore sdbp))
+    ;;(dotimes (i 10000) (backpropagation-batch sdbp data 0.1)) ;have unknown bugs
+    (levenberg-marquardt-backpropagation lmbp data) ;LMBP, result correct
+    ;;(dotimes (i 1000) (backpropagation sdbp data 0.2)) ;SDBP, result correct
+    ;;(loop for (input target) in data
+    ;;      do (format t "~&SDBP Training:~f ~,3f ~,3f~%" input (propagation-forward-without-states sdbp input) target))
+    (loop for (input target) in data
+          do (format t "~&LMBP Training:~f ~,3f ~,3f~%" input (propagation-forward-without-states lmbp input) target))
+    ))
