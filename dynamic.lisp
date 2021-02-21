155742b62a1e6b431d1d1a2fa3195e462c287cc9
hxz <herongxian@163.com>
2021-02-22 00:28:20 +0800

    Infinite Impulse Response Network Demonstration

--- dynamic.lisp
+++ dynamic.lisp
@@ -4,19 +4,38 @@
 
 (defun FIR-demo ()
   "Finite Impulse Response Network Demonstration. Page 272, Chinese edition."
-  (let ((tdl (make-tapped-delay-line 3 :init-element 2))
+  (let ((tdl (make-tapped-delay-line 3 :init-element 0))
         (weights '((1/3 1/3 1/3)))
         (bias 0)
         (transfer #'purelin))
-    (loop for i from 1 to 20
+    (loop for i from 0 to 19
           do (progn
-               (if (= (mod (- i (mod i 5)) 2) 0)
+               (if (= (mod (- i (mod i 5)) 2) 0) ;generate square-wave input signal
                    (input-tdl tdl 1)
                    (input-tdl tdl -1))
-               (format t "~&~d~d~d~%"
-                       i
-                       #\tab
-                       (funcall transfer
-                               (matrix-add (matrix-product weights
-                                                           (list-to-vector (get-tdl-contents tdl)))
-                                             bias)))))))
+               (let ((net-output (funcall transfer
+                                (matrix-add (matrix-product
+                                             weights
+                                             (list-to-vector (get-tdl-contents tdl)))
+                                            bias))))
+                 (format t "~&~d~d~,3f~%" i #\tab net-output))))))
+
+
+(defun IIR-demo ()
+  "Infinite Impulse Response Network Demonstration"
+  (let ((tdl (make-tapped-delay-line 2 :init-element 0))
+        (weights '((1/2 1/2)))
+        (bias 0)
+        (transfer #'purelin))
+    (loop for i from 0 to 19
+          do (progn
+               (if (= (mod (- i (mod i 5)) 2) 0) ;generate square-wave input signal
+                   (input-tdl tdl 1)
+                   (input-tdl tdl -1))
+               (let ((net-output (funcall transfer
+                                (matrix-add (matrix-product
+                                             weights
+                                             (list-to-vector (get-tdl-contents tdl)))
+                                            bias))))
+                 (input-tdl tdl net-output) ; the difference between fir-demo
+                 (format t "~&~d~d~,3f~%" i #\tab net-output))))))
