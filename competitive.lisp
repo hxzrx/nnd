2e8e3f54ac54acef4e2c658d0531962122ad0760
hxz <herongxian@163.com>
2021-03-11 22:57:27 +0800

    competitive with conscious

--- competitive.lisp
+++ competitive.lisp
@@ -17,7 +17,7 @@ the weights of the compet layer should be normalized first. samples is a list of
                (updated-weights (loop for cpt-res in (car (transpose compet-result))
                                       for neuro-weight in compet-layer-weight
                                       collect (if (< (abs (- cpt-res 1)) 0.00000001)
-                                                  (let ((weight-row (list neuro-weight))
+                                                  (let ((weight-row (list neuro-weight)) ;winner
                                                         (input-row (transpose input)))
                                                     (first (matrix-add (matrix-product (- 1 learning-rate) weight-row)
                                                                        (matrix-product learning-rate input-row))))
@@ -28,7 +28,61 @@ the weights of the compet layer should be normalized first. samples is a list of
           (setf (nth compet-layer-id weights) updated-weights)
           (format t "~&Updated to:~%~{~{~d~^ ~}~^~&~}~%~%" (nth compet-layer-id weights))
           ))))
+  (format t "Trained network: ~d~%" network)
   network)
+
+(defun check-dead-neurons (old-weights new-weights)
+  "a neuron is called deal if it's weight didn't change after training"
+  (format t "old weights:~&~{~d~^~&~}~&new weights:~&~{~d~^~&~}~%" old-weights new-weights)
+  (loop for w-old in old-weights
+        for w-new in new-weights
+        for index from 0
+        when (equal w-old w-new)
+          collect index))
+
+(defmethod competitive-learning-consicious ((network static-network) samples &optional (learning-rate 0.5))
+  "Kohonen rule: w(q) = w(q-1) + alpha * (p(q) - w(q-1)) for the winning neuron.
+the weights of the compet layer should be normalized first. samples is a list of sample of input vectors"
+  (let ((compet-layer-id 0)
+        (old-weights (copy-seq (get-weights network)))
+        (train-result (competitive-learning network samples learning-rate))
+        (time 0))
+    (alexandria:when-let (dead-neurons (check-dead-neurons (nth compet-layer-id old-weights)
+                                                           (nth compet-layer-id (get-weights train-result))))
+      (format t "~&Dead neurons found: ~{~d~^ ~}~&Conscious technique will be used next!~%" dead-neurons)
+      (with-slots ((weights weights)
+                   (biases biases)) network
+      (dolist (sample samples)
+          (incf time)
+          (let* ((input (first sample))
+                 (compet-layer-weight (first weights))
+                 (compet-layer-bias   (first biases))
+                 (compet-result (static-network-output network input))
+                 (updated-weights (loop for cpt-res in (car (transpose compet-result))
+                                        for neuro-weight in compet-layer-weight
+                                        collect (if (< (abs (- cpt-res 1)) 0.00000001)
+                                                    (let ((weight-row (list neuro-weight)) ;winner
+                                                          (input-row (transpose input)))
+                                                      (first (matrix-add (matrix-product (- 1 learning-rate) weight-row)
+                                                                         (matrix-product learning-rate input-row))))
+                                                    neuro-weight)))
+                 (updated-biases (loop for cpt-res in (car (transpose compet-result))
+                                       for neuro-bias in compet-layer-bias
+                                       collect (if (< (abs (- cpt-res 1)) 0.00000001)
+                                                   (list (- (car neuro-bias) 0.2)) ;winner
+                                                   (list (* 0.9 (car neuro-bias)))))))
+            (format t "~&Q=~d, compet result: ~{~d~^ ~}~%Weight:~%~{~{~d~^ ~}~^~&~}~&Biases:~&~{~{~d~^ ~}~^~&~}~%" time compet-result compet-layer-weight compet-layer-bias)
+
+            ;;CANNOT setf compet-layer-weight, that will leading to unpredictable results
+            (setf (nth compet-layer-id weights) updated-weights)
+            (setf (nth compet-layer-id biases) updated-biases)
+            (format t "~&Updated weights:~%~{~{~d~^ ~}~^~&~}~&Updated biases:~%~{~{~d~^ ~}~^~&~}~%~%" (nth compet-layer-id weights) (nth compet-layer-id biases))
+            ))))
+    (format t "Trained network: ~d~%" network)
+    network))
+
+
+
 
 
 
@@ -65,15 +119,37 @@ the weights of the compet layer should be normalized first. samples is a list of
         (samples (repeat-list (list (list '((-1) (0)))
                                       (list '((0) (1)))
                                       (list (list (list (/ 1 (sqrt 2))) (list (/ 1 (sqrt 2))))))
-                                2)
-                       ))
+                                2))
+        (learning-rate 0.5))
     (format t "Initial network:~&~d~%~%" network)
     (with-slots ((weights weights)) network ;normalize the compet layer's weight
       (let* ((compet-layer-weight (first weights)))
         (setf compet-layer-weight
               (loop for rows in compet-layer-weight
                     collect (first (normalize (list rows)))))))
-    (competitive-learning network samples 0.5)))
+    (competitive-learning network samples learning-rate)
+    ))
+
+(defun example-p15.2-page-316-conscious ()
+  (let ((network (make-static-network :neurons (list 2 3)
+                                      :weights (list (list (list 0 -1)
+                                                           (list (/ -2 (sqrt 5)) (/ 1 (sqrt 5)))
+                                                           (list (/ -1 (sqrt 5)) (/ 2 (sqrt 5)))))
+                                      :summers (list :sum)
+                                      :transfers (list #'compet)))
+        (samples (repeat-list (list (list '((-1) (0)))
+                                      (list '((0) (1)))
+                                      (list (list (list (/ 1 (sqrt 2))) (list (/ 1 (sqrt 2))))))
+                                10))
+        (learning-rate 0.5))
+    (format t "Initial network:~&~d~%~%" network)
+    (with-slots ((weights weights)) network ;normalize the compet layer's weight
+      (let* ((compet-layer-weight (first weights)))
+        (setf compet-layer-weight
+              (loop for rows in compet-layer-weight
+                    collect (first (normalize (list rows)))))))
+    (competitive-learning-consicious network samples learning-rate)
+    ))
 
 (defun exercise-e15.3-page-323 ()
   (let ((network (make-static-network :neurons (list 2 2)
