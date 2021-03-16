539270dcc725ea7a798a075ed54f1a42a467e55a
hxz <herongxian@163.com>
2021-03-16 16:53:57 +0800

    complete demo-page-336

--- rbf.lisp
+++ rbf.lisp
@@ -9,13 +9,12 @@
          (reg-matrix (make-augmented (loop for input in input-vecs ;regression matrix
                                            append (transpose input))
                                      (make-ones input-num 1)))
-         (reg-matrix^T (transpose reg-matrix))
-         (target-vector (transpose (list targets))))
-    (reduce #'matrix-product
-            (matrix-inverse (matrix-add (matrix-product reg-matrix^T reg-matrix)
-                                        (matrix-product rho (eye (1+ input-rank)))))
-            reg-matrix^T
-            target-vector)))
+         (reg-matrixᵀ (transpose reg-matrix))
+         (target-vector (transpose (list targets)))
+         (UᵀU (matrix-product reg-matrixᵀ reg-matrix))
+         (rho-eye (matrix-product rho (eye (1+ input-rank))))
+         (inv (matrix-inverse (matrix-add UᵀU rho-eye))))
+    (reduce #'matrix-product (list inv reg-matrixᵀ target-vector))))
 
 
 
@@ -51,14 +50,20 @@
           do (format t "~%Input: ~d~&Output:~&~d~%" in (static-network-output! network in)))))
 
 (defun demo-page-336 ()
-  "Chinese Edition, this example demonstrates the output of the 1st layer"
-  (let ((network (make-static-network :neurons (list 1 3)
-                                      :weights (list '((-2) (0) (2)))
-                                      :biases (list '((0.5) (0.5) (0.5)))
-                                      :input-proc (list :|dist|)
-                                      :bias-proc (list :.*)
-                                      :transfers (list #'radbas)))
-        (inputs '(-2 -1.2 -0.4 0.4 1.2 2)))
+  "Chinese Edition, this example demonstrates the linear least squares utilized to set the parameters in layer2.
+note that there are differences between the results of this demo with those in the textbook, and they are due to the instability of the inverse of the matrix"
+  (let* ((network (make-static-network :neurons (list 1 3)
+                                       :weights (list '((-2) (0) (2)))
+                                       :biases (list '((0.5) (0.5) (0.5)))
+                                       :input-proc (list :|dist|)
+                                       :bias-proc (list :.*)
+                                       :transfers (list #'radbas)))
+         (inputs '(-2 -1.2 -0.4 0.4 1.2 2))
+         (targets '(0 0.19 0.69 1.3 1.8 2))
+         (layer-id 0)
+         (neuron-outputs (loop for in in inputs
+                               collect (let ((output (static-network-output! network in)))
+                                         (declare (ignore output))
+                                         (get-neuron-outputs network layer-id)))))
     (format t "Initial network:~&~d~%~%" network)
-    (loop for in in inputs
-          do (format t "~%Input: ~d~&Output:~&~d~%" in (static-network-output! network in)))))
+    (linear-least-squares neuron-outputs targets)))
