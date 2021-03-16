112b44eaa21ea51d9bb2f75d07a8d79e2b160ac9
hxz <herongxian@163.com>
2021-03-16 13:52:01 +0800

    demo-page-332

--- rbf.lisp
+++ rbf.lisp
@@ -10,13 +10,25 @@
 ;;;; examples and exercises
 
 (defun demo-page-330 ()
-  (let ((network (make-static-network :neurons (list 1 2)
+  (let ((network (make-static-network :neurons (list 1 2 1)
                                       :weights (list '((-1) (1)) '((1 1)))
                                       :biases (list '((2) (2)) 0)
                                       :input-proc (list :|dist| :*)
                                       :bias-proc (list :.* :+)
                                       :transfers (list #'radbas #'purelin)))
         (inputs '(-2 -1 0 1 2)))
+    (format t "Initial network:~&~d~%~%" network)
+    (loop for in in inputs
+          do (format t "~%Input: ~d~&Output:~&~d~%" in (static-network-output! network in)))))
+
+(defun demo-page-332 ()
+  (let ((network (make-static-network :neurons (list 2 2 1)
+                                      :weights (list '((-1 1) (1 -1)) '((2 2)))
+                                      :biases (list '((1) (1)) -1)
+                                      :input-proc (list :|dist| :*)
+                                      :bias-proc (list :.* :+)
+                                      :transfers (list #'radbas #'purelin)))
+        (inputs '(((-1) (1)) ((1) (-1)) ((-1) (-1)) ((1) (1)))))
     (format t "Initial network:~&~d~%~%" network)
     (loop for in in inputs
           do (format t "~%Input: ~d~&Output:~&~d~%" in (static-network-output! network in)))))
